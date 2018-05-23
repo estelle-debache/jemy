4d75a09d4332560c67761661b75c2f374113f02e
estelle-debache <estelle.debache@gmail.com>
2018-05-23 13:08:23 +0200

    estelle1

---
+++ src/Entity/Candidature.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CandidatureRepository")
+ */
+class Candidature
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $cv;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $motivation;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateCandidature;
+
+    /**
+     * @ORM\JoinColumn(nullable=false)
+     * @ORM\ManyToOne(targetEntity="OffreEmploi", inversedBy="candidature")
+     */
+    private $offreEmploi;
+    
+    /**
+     * @ORM\JoinColumn(nullable=false)
+     * @ORM\ManyToOne(targetEntity="OffreEmploi", inversedBy="candidature")
+     */
+    private $salarie;
+    
+    public function getSalarie() {
+        return $this->salarie;
+    }
+
+    public function setSalarie($salarie) {
+        $this->salarie = $salarie;
+        return $this;
+    }
+
+        public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getCv()
+    {
+        return $this->cv;
+    }
+
+    public function setCv( $cv)
+    {
+        $this->cv = $cv;
+
+        return $this;
+    }
+
+    public function getMotivation()
+    {
+        return $this->motivation;
+    }
+
+    public function setMotivation( $motivation)
+    {
+        $this->motivation = $motivation;
+
+        return $this;
+    }
+
+    public function getDateCandidature()
+    {
+        return $this->dateCandidature;
+    }
+
+    public function setDateCandidature( $dateCandidature)
+    {
+        $this->dateCandidature = $dateCandidature;
+
+        return $this;
+    }
+
+    public function getOffreEmploi()
+    {
+        return $this->offreEmploi;
+    }
+
+    public function setOffreEmploi( $offreEmploi)
+    {
+        $this->offreEmploi = $offreEmploi;
+
+        return $this;
+    }
+}
