7f60189c44df4e838b5324554d9bab4e7d5b7640
estelle-debache <estelle.debache@gmail.com>
2018-05-28 12:18:13 +0200

    OK CORRECTION IMAGE SUR NEWS

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -302,7 +302,7 @@ class AdminController extends Controller
             //nom du fichier en bdd
                 $originalImage = $actualite->getImage();
                 $actualite->setImage(
-                        new File($this->getParameter('upload_dir') . $originalImage)
+                        new File($this->getParameter('upload_dir'). "/" . $originalImage)
                  );
             }
         }
@@ -345,7 +345,7 @@ class AdminController extends Controller
                     // s'il on est en modification d'un article qui en avait
                     // déjà une 
                     if(!is_null($originalImage)){
-                        unlink($this->getParameter('upload_dir') . $originalImage);
+                        unlink($this->getParameter('upload_dir'). "/" . $originalImage);
                     }
                      
                  }else{
